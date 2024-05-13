facaa41253a84987903845b7f16ea5028aac78e0
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-05-13 16:27:43 +0700

    config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["1004206704994566164", "1229076807379194041"], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "00fbff",
